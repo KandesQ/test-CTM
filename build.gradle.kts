608ef40dfd17020898765279b18218d975f602bb
Fedor Kapranov <fedor.kapranov.06@mail.ru>
2025-03-29 14:05:16 +0300

    fix jooq-starter version problem

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,100 +1,104 @@
 plugins {
-	java
-	id("org.springframework.boot") version "3.4.4"
-	id("io.spring.dependency-management") version "1.1.7"
-	id("org.jooq.jooq-codegen-gradle") version "3.20.1" // 3.19.21 сгенерировалось но я хз будет ли работать нормально
-	// проблема в том что org.jooq.jooq-codegen-gradle последней версии видимо не поддерживал foreignKeyRule (ON UPDATE/DELETE которые)
-	// поэтому все падало с ошибкой что такого класса он не знает
-	// но вместе с ошибкой пропал код который был с ForeignKeyRule связан (указания что делать при удалении связанной сущности)
+    java
+    id("org.springframework.boot") version "3.4.4"
+    id("io.spring.dependency-management") version "1.1.7"
+     id("org.jooq.jooq-codegen-gradle") version "3.20.1" // 3.19.21 сгенерировалось но я хз будет ли работать нормально
+    // проблема в том что org.jooq.jooq-codegen-gradle последней версии видимо не поддерживал foreignKeyRule (ON UPDATE/DELETE которые)
+    // поэтому все падало с ошибкой что такого класса он не знает
+    // но вместе с ошибкой пропал код который был с ForeignKeyRule связан (указания что делать при удалении связанной сущности)
 
-	/**
-	 * import org.jooq.impl.QOM.ForeignKeyRule - c этим классом жук не понимает что делать,
-	 * потому что версия генератора и самого жука слишком разные (молодая и старая)
-	 *
-	 * Еще одна возможная проблема (70%) - стартер подтягивает старую версию жука,
-	 * поэтому она не может понять что делать с кодом, который сгенерил codegen-gradle 3.20.2 версии
-	 * Короче спринг бут зависимость слишком старая, кодген слишком новый
-	 * Возможное решение - как то переопределить версии жука и его зависимостей, которые подтягивает стартер
-	 */
+    /**
+     * import org.jooq.impl.QOM.ForeignKeyRule - c этим классом жук не понимает что делать,
+     * потому что версия генератора и самого жука слишком разные (молодая и старая)
+     *
+     * Проблема - стартер подтягивает старую версию жука,
+     * поэтому она не может понять что делать с кодом, который сгенерил codegen-gradle 3.20.2 версии
+     * Короче спринг бут зависимость слишком старая, кодген слишком новый
+     * Возможное решение - как то переопределить версии жука и его зависимостей, которые подтягивает стартер
+     * или вручную добавить артефакты жука
+     */
 
 }
 
 repositories {
-	mavenCentral()
+    mavenCentral()
 }
 
 group = "com.example"
 version = "0.0.1-SNAPSHOT"
 
 java {
-	toolchain {
-		languageVersion = JavaLanguageVersion.of(23)
-	}
+    toolchain {
+        languageVersion = JavaLanguageVersion.of(23)
+    }
 }
 
 jooq {
-	configuration {
-		jdbc {
-			driver = "org.postgresql.Driver"
-			url = "jdbc:postgresql://localhost:5432/appdatabase"
-			user = "postgres"
-			password = "root"
-		}
-		generator {
-			name = "org.jooq.codegen.DefaultGenerator"
-			database {
-				name = "org.jooq.meta.postgres.PostgresDatabase"
-				inputSchema = "public"
-				includes = ".*"
-			}
-			target {
-				packageName = "com.test_CTM.jooq.generated"
-				directory = "src/main/java/generated"
-			}
-		}
-	}
+    configuration {
+        jdbc {
+            driver = "org.postgresql.Driver"
+            url = "jdbc:postgresql://localhost:5432/appdatabase"
+            user = "postgres"
+            password = "root"
+        }
+        generator {
+            name = "org.jooq.codegen.DefaultGenerator"
+            database {
+                name = "org.jooq.meta.postgres.PostgresDatabase"
+                inputSchema = "public"
+                includes = ".*"
+            }
+            target {
+                packageName = "com.test_CTM.jooq.generated"
+                directory = "src/main/java/generated"
+            }
+        }
+    }
 }
 
 sourceSets {
-	main {
-		java {
-			srcDirs("src/main/java", "src/main/generated")
-		}
-	}
+    main {
+        java {
+            srcDirs("src/main/java")
+        }
+    }
 }
 
 configurations {
-	compileOnly {
-		extendsFrom(configurations.annotationProcessor.get())
-	}
+    compileOnly {
+        extendsFrom(configurations.annotationProcessor.get())
+    }
 }
 
 repositories {
-	mavenCentral()
-	gradlePluginPortal()
+    mavenCentral()
+    gradlePluginPortal()
 }
 
 
 dependencies {
-		implementation("org.springframework.boot:spring-boot-starter-jooq")
+//    implementation("org.springframework.boot:spring-boot-starter-jooq")
+    implementation("org.jooq:jooq:3.20.2")
+    implementation("org.jooq:jooq-codegen:3.20.2")
+    implementation("org.jooq:jooq-meta:3.20.2")
 
-	implementation("org.springframework.boot:spring-boot-starter-web")
-	implementation("org.mapstruct:mapstruct:1.5.3.Final")
-	implementation("org.mapstruct:mapstruct-processor:1.5.3.Final")
-	implementation("org.liquibase:liquibase-core:4.24.0")
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.mapstruct:mapstruct:1.5.3.Final")
+    implementation("org.mapstruct:mapstruct-processor:1.5.3.Final")
+    implementation("org.liquibase:liquibase-core:4.24.0")
 
-	compileOnly("org.projectlombok:lombok")
+    compileOnly("org.projectlombok:lombok")
 
-	runtimeOnly("com.h2database:h2")
-	runtimeOnly("org.postgresql:postgresql")
+    runtimeOnly("com.h2database:h2")
+    runtimeOnly("org.postgresql:postgresql")
 
-	annotationProcessor("org.projectlombok:lombok")
-	testImplementation("org.springframework.boot:spring-boot-starter-test")
-	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+    annotationProcessor("org.projectlombok:lombok")
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 
-	jooqCodegen("org.postgresql:postgresql:42.7.5")
+    jooqCodegen("org.postgresql:postgresql:42.7.5")
 }
 
 tasks.withType<Test> {
-	useJUnitPlatform()
+    useJUnitPlatform()
 }
